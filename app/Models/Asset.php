76f5cacb36443030a6b970e19c8f712233b66c38
Tomas Verhelst <>
2021-05-05 21:00:42 +0200

    Removed Spatie's media library

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -2,20 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Asset extends Model implements HasMedia
+class Asset extends Model
 {
-    use InteractsWithMedia;
-
-    public function registerMediaCollections(): void
-    {
-        $this->addMediaCollection('assets');
-
-    }
 
     protected $guarded = ['id'];
 
